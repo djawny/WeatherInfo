9e8ac3ec5ac8a7f6ca60d03f06e26d7c7c8a4631
djawny <djawny@op.pl>
2017-05-04 19:07:51 +0200

    model

---
+++ app/src/main/java/com/example/daniel/weatherinfo/application/WeatherApplication.java
@@ -0,0 +1,12 @@
+package com.example.daniel.weatherinfo.application;
+
+import android.app.Application;
+
+public class WeatherApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
