9eed69e37b9b6988756e1eb20e67802aa9586460
djawny <djawny@op.pl>
2017-05-08 13:20:58 +0200

    mvp

---
+++ app/src/main/java/com/example/daniel/weatherinfo/util/Preconditions.java
@@ -0,0 +1,18 @@
+package com.example.daniel.weatherinfo.util;
+
+public final class Preconditions {
+
+    public static <T> T checkNotNull(T reference) {
+        if (reference == null) {
+            throw new NullPointerException();
+        }
+        return reference;
+    }
+
+    public static <T> T checkNotNull(T reference, String message) {
+        if (reference == null) {
+            throw new NullPointerException(message);
+        }
+        return reference;
+    }
+}
